cb6cab22b3270a4b33bbfba1235c0e819f2d6863
lingting <sunlisten.gzm@gmail.com>
2024-03-13 10:50:13 +0800

    :zap: 实现 P1047

--- P1047.c
+++ P1047.c
@@ -4,40 +4,36 @@
 /**
  * https://www.luogu.com.cn/problem/P1047
  */
-int main()
-{
-    int a;
-    int b;
-    
-    scanf_s("%d", &a);//输入一个数 代表总数a
-    
-    scanf_s("%d", &b);//输入一个数 代表区域数量b
-    int c = 2 * b;
-    int* arr = (int*)malloc(c * sizeof(int));//为数组arr创建它所需要的大小的内存池
-    
-    for (int i =0; i < c; i+=2) {
-        scanf_s("%d %d", &arr[i], &arr[i + 1]);
-        printf("\n");
-        
-    }//使输入数据每两个数据为一组
-    if (arr == NULL) {
+int main() {
+    // 定义马路长度和区域数量
+    int len, count;
+    scanf_s("%d %d", &len, &count);
+    if (count < 1) {
         printf("nothing");
+        return 0;
     }
-    for (int i = c; i >0; i--) {
-        for (int j = 0; j < c; j++) {
-            if (arr[j] > arr[j + 1]) {
-                int d;
-                d = arr[j];
-                arr[j] = arr[j + 1];
-                d = arr[j + 1];
+
+    // 定义树的总数
+    int max = len + 1;
+    // 统计剩余树数量
+    int surplus = max;
+    // 定义存储需要移除的树标志位
+    int flags[max] = {};
+
+    for (int i = 0; i < count; ++i) {
+        int start, end;
+        scanf_s("%d %d", &start, &end);
+
+        for (int j = start; j <= end; ++j) {
+            // 根据坐标进行判断
+            if (flags[j] == 0) {
+                // 如果该树未移除则 移除且标记
+                surplus--;
+                flags[j] = 1;
             }
         }
-    }//使用冒泡法排序得到的数组
-    
-        int f = 0;
-        f = a - (arr[c] - arr[0] + 1);
-    
-    printf("%d", f);
+    }
 
+    printf("%d", surplus);
     return 0;
 }
