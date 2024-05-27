c713d5ee5586bd346baf257d1262913ef91df547
lingting <sunlisten.gzm@foxmail.com>
2024-05-27 16:31:30 +0800

    :bug: 修复 p1161 输出不符合预期

--- p1161.c
+++ p1161.c
@@ -15,8 +15,6 @@ int main() {
      * 使用 calloc 动态申请分配. 规避静态申请可能栈溢出问题
     */
     int *lamps = (int *) calloc(2000001, sizeof(int));
-    // 存放最后一个奇数操作的索引
-    int lastOdd = 0;
 
     for (int i = 0; i < n; ++i) {
         double a;
@@ -25,14 +23,18 @@ int main() {
 
         for (int j = 1; j <= t; ++j) {
             int index = (int) floor(j * a);
-            lamps[index] ^= 1;  // 使用异或操作翻转灯的状态
-            if (lamps[index] == 1) {
-                lastOdd = index;
-            }
+            // 使用异或操作翻转灯的状态
+            lamps[index] ^= 1;
         }
     }
 
-    printf("%d", lastOdd);
+    for (int i = 0; i < 2000001; ++i) {
+        if (lamps[i] == 1) {
+            printf("%d", i);
+            break;
+        }
+    }
+
     free(lamps);
     return 0;
 }
